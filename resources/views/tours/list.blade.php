ff1b40e7a69b2b4c22ae180571edcd628e668a9f
noa <noa.duchiron@etu.unilim.fr>
2023-10-19 11:03:05 +0200

    Add trigger to update tour in list page

--- resources/views/tours/list.blade.php
+++ resources/views/tours/list.blade.php
@@ -13,7 +13,7 @@
             <div class="col-3" style="background-color: #D5D5D5FF">
                 <p class="font-semibold">NOM</p>
             </div>
-            <div class="col-3">
+            <div class="col-2">
                 <p class="font-semibold">DATE</p>
             </div>
             <div class="col-3" style="background-color: #D5D5D5FF">
@@ -24,13 +24,12 @@
             </div>
         </div>
 
-
-        <div class="row">
-            @foreach ($tours as $tour)
+        @foreach ($tours as $tour)
+            <div class="row">
                 <div class="col-3" style="background-color: #D5D5D5FF">
                     <p>{{ $tour->user->name }}</p>
                 </div>
-                <div class="col-3">
+                <div class="col-2">
                     <p>{{ $tour->date }}</p>
                 </div>
                 <div class="col-3" style="background-color: #D5D5D5FF">
@@ -39,8 +38,36 @@
                 <div class="col-3">
                     <p>{{ $tour->car }}</p>
                 </div>
-            @endforeach
-        </div>
+                <div class="col-1">
+                    @if ($tour->user->is(auth()->user()))
+                        <x-dropdown>
+                            <x-slot name="trigger">
+                                <button>
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400"
+                                         viewBox="0 0 20 20" fill="currentColor">
+                                        <path
+                                            d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"/>
+                                    </svg>
+                                </button>
+                            </x-slot>
+                            <x-slot name="content">
+                                <x-dropdown-link :href="route('tours.edit', $tour)">
+                                    {{ __('Modifier') }}
+                                </x-dropdown-link>
+                                <form method="POST" action="{{ route('tours.destroy', $tour) }}">
+                                    @csrf
+                                    @method('delete')
+                                    <x-dropdown-link :href="route('tours.destroy', $tour)"
+                                                     onclick="event.preventDefault(); this.closest('form').submit();">
+                                        {{ __('Supprimer') }}
+                                    </x-dropdown-link>
+                                </form>
+                            </x-slot>
+                        </x-dropdown>
+                    @endif
+                </div>
+            </div>
+        @endforeach
 
 
         {{ $tours->links() }}
